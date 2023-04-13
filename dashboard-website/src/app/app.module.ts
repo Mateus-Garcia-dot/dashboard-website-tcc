982086ad5709cef2cc6bc5d7c9e3e3b4819a84e2
Fernanda Moraes Costa <fer-mcosta@hotmail.com>
2023-04-13 19:48:04 -0300

    Adicionado navbar ao projeto

--- dashboard-website/src/app/app.module.ts
+++ dashboard-website/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapaModule } from './shared/mapa/mapa.module';
+import { NavbarModule } from './components/navbar/navbar.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MapaModule } from './shared/mapa/mapa.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MapaModule
+    MapaModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
